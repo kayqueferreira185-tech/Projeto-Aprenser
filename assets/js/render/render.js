7686443c3e360784c764987a064d05c067be2d13
kayque lima <kayqueferreira185@gmail.com>
2026-08-10 19:42:26 -0300

    feat: implementa alterações finais na aba de cursos e alterações gerais no site

--- assets/js/render/render.js
+++ assets/js/render/render.js
@@ -5,19 +5,28 @@ export function renderizarCursos(cursos){
     cursos.forEach(curso => {
         const article = document.createElement('article');
         article.classList.add('produto-curso');
+
         const imagemCurso = document.createElement('img');
         imagemCurso.setAttribute('alt', 'Imagem do curso');
-        imagemCurso.classList.add('img.curso');
+        imagemCurso.classList.add('img-curso');
         imagemCurso.src = curso.imagem;
+
+        const conteudoCurso = document.createElement('div');
+        conteudoCurso.classList.add('conteudo-curso');
+
         const tituloCurso = document.createElement('h3');
-        tituloCurso.textContent = curso.titulo
+        tituloCurso.textContent = curso.titulo;
+        tituloCurso.classList.add('titulo-item-curso');
+
         const descricaoCurso = document.createElement('p');
         descricaoCurso.textContent = curso.descricao;
 
-        article.appendChild(imagemCurso);
-        article.appendChild(tituloCurso);
-        article.appendChild(descricaoCurso);
-        listaCursos.appendChild(article);
+        conteudoCurso.appendChild(tituloCurso);
+        conteudoCurso.appendChild(descricaoCurso);
 
+        article.appendChild(imagemCurso);
+        article.appendChild(conteudoCurso);
+
+        listaCursos.appendChild(article);
     });
 }
